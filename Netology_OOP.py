aa0fcfaa17ab5285e78826c88d3fc5001697500e
Ivan Golovinov <topclassprogrammer@gmail.com>
2023-10-30 11:53:35 +0300

    Added Lecturer class, Reviewer class and rate_lecturer in Student class

---
+++ Netology_OOP.py
@@ -0,0 +1,66 @@
+class Student:
+
+    def __init__(self, name, surname, gender):
+        self.name = name
+        self.surname = surname
+        self.gender = gender
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
+
+    def rate_lecturer(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and self.courses_in_progress in lecturer.courses_attached:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+
+class Mentor:
+
+    def __init__(self, name, surname, gender):
+        self.name = name
+        self.surname = surname
+        self.gender = gender
+        self.courses_attached = []
+
+
+class Lecturer(Mentor):
+
+    def __init__(self, name, surname, gender):
+        super().__init__(name, surname, gender)
+        self.grades = {}
+
+
+class Reviewer(Mentor):
+
+    def __init__(self, name, surname, gender):
+        super().__init__(name, surname, gender)
+
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+
+
+
+
+
+# best_student = Student('Ruoy', 'Eman', 'your_gender')
+# best_student.courses_in_progress += ['Python']
+#
+# cool_mentor = Mentor('Some', 'Buddy')
+# cool_mentor.courses_attached += ['Python']
+#
+# cool_mentor.rate_hw(best_student, 'Python', 10)
+# cool_mentor.rate_hw(best_student, 'Python', 10)
+# cool_mentor.rate_hw(best_student, 'Python', 10)
+#
+# print(best_student.grades)
